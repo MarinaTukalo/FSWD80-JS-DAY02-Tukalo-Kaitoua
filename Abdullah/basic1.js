7fa2b7a09b9e09372c34066a621c6408d0a60b07
Kaitoua <abdullah.kaitoua@gmail.com>
2019-09-23 15:00:19 +0200

    file added

---
+++ Abdullah/basic1.js
@@ -0,0 +1,11 @@
+function crystalGazer(jobtitle, location, partners_name, children_number)
+{
+
+	var jobtitle = "web developer";
+
+
+   return "You have " + jobtitle + " messages.";
+}
+
+crystalGazer();
+
